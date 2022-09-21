8a5dc1a15bb409a9ff9a9c1150f0cba83a5be6c1
matthew-lui <107003143+matthew-lui@users.noreply.github.com>
2022-09-21 14:46:19 -0400

    commit tree ac802634fcdba761c21d8afebac8b235bd7d83ab

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400){
+    return 'This one is on me!'
+  }
+  else if (distance <= 2000 && 400){
+    return 'That will be twenty bucks.'
+  }
+  else if (distance >= 2000 && distance <= 2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (distance >= 2501){
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination){
+  return destination === 'NYC' ? 'Ok, sounds good.' : `No go.`
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous' :
+      return 'Thank you.'
+    case'thanks for everything' :
+      return 'Bye.'
+  }
 }
